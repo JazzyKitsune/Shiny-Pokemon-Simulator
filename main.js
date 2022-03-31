4cfbd8bdcf5645258dbec5aa4903e571ddf02a8c
Zoe <zmettauer@icloud.com>
2022-03-30 19:58:59 -0600

    Done I think
    got it working, all console stuff, doesn't really make a difference

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-function getRandomInt(max) {
+function randomInt(max) {
     return Math.floor(Math.random() * max)
 }
 
-let tID = prompt("Trainer ID? 0-65535 (0xFFFF)")    // Trainer ID - 16 bit int
-let sID = prompt("Secret ID? 0-65535 (0xFFFF)")     // Secret ID - 16 bit int
- 
-console.log(tID)
-console.log(sID)
+let tID = 0                 // Trainer ID - 16 bit int
+let sID = 0                 // Secret ID - 16 bit int
+let tSV = tID^sID           // Trainer Shiny Value 
+let pID = 0                 // Pokemon's Personality ID - 32 bit int
+let pSV = 0                 // Pokemon Shiny Value
+let shiny = 0               // Shiny Value, if shiny < 8 then the Pokemon is Shiny
+let counter = 0             // How many times trough the loop?
+let odds = 8                // Shiny odds, shiny < odds = shiny pokemon, defaults to 8 (gens 3-5) or 16 (gens 6-8)
 
-let tSV = tID^sID
-let pID = 0
-let pSV = 0
-let shiny = 0
-let counter = 0
+tID = prompt("Trainer ID? 0-65535")
+sID = prompt("Secret ID? 0-65535")
+odds = prompt("Shiny odds? 8 for 1/8192, 16 for 1/4096. Max is 65535")
 
-/* do {
-    pID = getRandomInt(0xFFFFFFFF)
-    pSV = (pID&0xFFFF)^(pID>>16)
-    shiny = tSV^pSV
+do {
+    pID = randomInt(0xFFFFFFFF)
+    pSV = (pID & 0xFFFF)^(pID >>> 16)
+    shiny = tSV ^ pSV
     console.log(shiny)
     counter++
-    console.log(counter)
-} while(shiny<8) */
-
-pID = getRandomInt(0xFFFFFFFF)
-console.log(pID)
-console.log(pID & 0xFFFF)
-console.log(pID >> 16)
-pSV = (pID&0xFFFF)^(pID>>16)
-console.log(pSV)
+    console.log("count = " + counter)
+    console.log("")
+} while(shiny >= odds)
+console.log("shiny!")
+if(shiny==0)
+{console.log("square sparkles")
+}

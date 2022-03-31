f0dfb73594d2cbe82215141b4cbbe01a9057bba3
Zoe <zmettauer@icloud.com>
2022-03-31 12:25:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ function calculateShiny(trID, srID, ppV) {
 let tID = 0                 // Trainer ID - 16 bit int
 let sID = 0                 // Secret ID - 16 bit int
 let pID = 0                 // Pokemon's Personality ID - 32 bit int
-let pSV = 0                 // Pokemon Shiny Value
 let shiny = 0               // Shiny Value, if shiny < 8 then the Pokemon is Shiny
 let counter = 0             // How many times trough the loop?
 let odds = 8                // Shiny odds, shiny < odds = shiny pokemon, defaults to 8 (gens 3-5) or 16 (gens 6-8)
